10ba449ec4bbb5f9455eb2759b1b236cf1a03290
Linotech Solutions <linotechsolutions11@gmail.com>
2025-10-31 12:15:35 +0200

    Updated Supervision Module with Neon DB

--- services/supervision_module/sql/create_database.sql
+++
@@ -1,4 +0,0 @@
--- Run this connected to the 'postgres' database as a superuser (e.g., user 'postgres')
--- Creates the application database used by SECZim-AML-Guard supervision module
-
-CREATE DATABASE amlguard;
